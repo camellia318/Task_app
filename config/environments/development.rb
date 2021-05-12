922d71edf7c287772b84f316b2f2383f1274e25d
EC2 Default User <ec2-user@ip-172-31-6-182.ap-northeast-1.compute.internal>
2021-05-12 13:30:10 +0000

    変更されたファイルを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+  config.hosts << "d92fe88cf811457595904edcc2965c08.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks', to: 'tasks#index'
+  
+  get 'tasks/add'
+  post 'tasks/add'
+  
+  get 'tasks/:id', to: 'tasks#show'
+  #get 'tasks/show'
+  
+  get 'tasks/edit/:id', to: 'tasks#edit'
+  patch 'tasks/edit/:id', to: 'tasks#edit'
+  
+  get 'tasks/delete/:id', to: 'tasks#delete'
+  
+  resources :posts, constraints: { format: 'html' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
